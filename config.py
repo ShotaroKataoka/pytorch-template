6e726d854e5adca959a405f16df677e670df567b
Shotaro Kataoka <e5206@gm.kochi-ct.jp>
2019-09-26 14:39:17 +0900

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config(object):
+    # general setting
+    input_dir = "./sample_data/"
+    output_dir = "./run/"
+    
+    # model setting
+    input_channel = 3
+    output_channel = 5
+    
+    # training setting
+    batch_size = 5
+    epoch = 2
